bcfd747f3d78c598603b445cf81c02af4e58f44b
robiulalamdev <robiulalam762400@gmail.com>
2024-01-20 01:02:12 +0600

    langding page hero section schema done

---
+++ modules/landing/landing.model.js
@@ -0,0 +1,21 @@
+const { Schema } = require("mongoose");
+
+const landingSchema = new Schema(
+  {
+    hero_section: {
+      type: Object,
+      heading: String,
+      sub_heading: String,
+      heading_summary: String,
+      lets_talk_now: String,
+      how_we_work: String,
+      banners: [String],
+      required: false,
+    },
+    companies: {
+      type: [String],
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
